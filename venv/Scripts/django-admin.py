8c19efdeca2b0fb91135286f86d78a3d19c359aa
Y-Y1Q <sh4bee@outlook.com>
2024-05-14 21:04:25 -0700

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\( SFSU\( 2024 Spring\CSC 651\project\sys-admin-demo\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
