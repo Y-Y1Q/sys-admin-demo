45897e5d246df70bfc71f66a577438a66ec9d3f0
Y-Y1Q <sh4bee@outlook.com>
2024-05-14 22:14:39 -0700

    test2

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
