723211f97072f3937cee4529384f9398aab62693
Y-Y1Q <143047725+Y-Y1Q@users.noreply.github.com>
2024-05-14 21:12:22 -0700

    Delete venv directory

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\( SFSU\( 2024 Spring\CSC 651\project\sys-admin-demo\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
